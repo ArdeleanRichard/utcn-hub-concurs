4e0a8887d7199f158e21ed783b126db90ff6fea0
cameliav <Camelia.Lemnaru@cs.utcluj.ro>
2025-01-17 20:06:44 +0100

    update index.php

---
+++ index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="ro">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>HUB UTCN – Terase Înverzite</title>
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
+</head>
+<body>
+    <header>
+       <img class="logo" src="./data/logo-utcn.png" alt="" />
+        <div>
+            <h1>HUB UTCN: Terase Înverzite</h1>
+            <nav>
+                <ul>
+                    <li><a href="index.html">Acasă</a></li>
+                    <li><a href="concurs.html">Concurs</a></li>
+                    <li><a href="detalii.html">Detalii</a></li>
+                    <li><a href="cerinte.html">Cerințe</a></li>
+                    <li><a href="calendar.html">Calendar</a></li>
+                    <li><a href="start.html">Vreau să particip</a></li>
+                </ul>
+            </nav>
+        </div>
+         <img class="logo" src="./data/logo-usamv.webp" alt="" />
+    </header>
+    <main>
+        <section class="intro">
+            <h2>Bun venit la concursul "Terasele HUB UTCN"</h2>
+            <p>Descoperiți cum puteți transforma terasele clădirii HUB UTCN în spații verzi cu o ambianță atractivă. Acest concurs este o oportunitate de colaborare interdisciplinară pentru studenți de la UTCN și USAMV.</p>
+            <a href="concurs.html" class="cta">Aflați mai multe</a>
+        </section>
+    </main>
+    <footer>
+        <p>&copy; 2025 HUB UTCN. Toate drepturile rezervate.</p>
+    </footer>
+</body>
+</html>
